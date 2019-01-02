87f04be83f7ac030ecd81cadd972908307a56b89
Wayne Hoover <wayne.hoover@legitscript.com>
2019-01-01 18:31:32 -1000

    better defaults

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({customCSS: 'h1{color: #fff}', customHTML: '<h1>I am happy, heathly and strong.</h1>'}, function() {
-      console.log("Defaults set for new tab page.");
-    });
-  });
+let css = `
+  h1{
+    text-align: center;
+    margin-top: 112px;
+    color:#fff;
+    font-family: SF Pro Display;
+    font-size: 40px;
+    font-weight: 400;
+    letter-spacing: 2px;
+  }`
+let html = `
+  <h1>I am strong, happy and heathly</h1>
+`
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({customCSS: css, customHTML: html});
+});
